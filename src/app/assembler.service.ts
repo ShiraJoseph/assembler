7d30a8498abb7ef91ef5fb6d6a92004e8bea5cc7
Shira Joseph <sjoseph@catalyte.io>
2020-10-11 18:30:31 -0400

    Completed Angular Assembler

--- src/app/assembler.service.ts
+++ src/app/assembler.service.ts
@@ -6,38 +6,35 @@ import { Injectable } from '@angular/core';
 export class AssemblerService {
   assemblyCode: string[];
   instructions: string[];
+  labels: { key, value }[];
+  variables: { key, value }[];
   machineCode: string;
 
-  constructor() {
-  }
-
   assemble(blob) {
-    this.assemblyCode = this.removeComments(blob.split('\n'));
-    this.parseLabels();
-    this.parseSymbols();
-    this.machineCode = this.instructions.join('\n');
+    this.refineAssemblyCode(blob);
+    this.translate();
     return this.machineCode;
   }
 
-  private removeComments(arr: string[]) {
-    return arr.map((line: string) => {
+  private refineAssemblyCode(blob) {
+    this.assemblyCode = blob.split('\n');
+    this.removeComments();
+    this.parseLabels();
+    this.parseSymbols();
+    this.parseVariables();
+  }
+
+  private removeComments() {
+    this.assemblyCode = this.assemblyCode.map((line: string) => {
       return line.slice(0, line.indexOf('/') > -1 ? line.indexOf('/') : line.length).trim();
     }).filter(line => line);
   }
 
   private parseLabels() {
-    const symbols = [];
-    const code = [];
-    this.assemblyCode.forEach(line => {
-      if (line.startsWith('(')) {
-        symbols.push({label: line.replace(/[()]/g, ''), value: code.length});
-      } else {
-        code.push(line);
-      }
-    });
-    this.instructions = code.map(line => {
+    this.collectLabels();
+    this.instructions = this.instructions.map(line => {
       if (line.startsWith('@')) {
-        const reference = symbols.find(symbol => line.slice(1) === symbol.label);
+        const reference = this.labels.find(label => line.slice(1) === label.key);
         if (reference) {
           return `@${ reference.value }`;
         }
@@ -46,28 +43,25 @@ export class AssemblerService {
     });
   }
 
+  private collectLabels() {
+    this.labels = [];
+    this.instructions = [];
+    this.assemblyCode.forEach(line => {
+      if (line.startsWith('(')) {
+        this.labels.push({key: line.replace(/[()]/g, ''), value: this.instructions.length});
+      } else {
+        this.instructions.push(line);
+      }
+    });
+  }
+
   private parseSymbols() {
     this.instructions = this.instructions.map(instruction => {
-      if (instruction.match(/[@][a-bA-B]/g)) {
+      if (instruction.match(/[@][a-zA-Z]+/g)) {
+        if (instruction.slice(1).match(/[R][0-15]/g)) {
+          return `@${ instruction.slice(2) }`;
+        }
         switch (instruction.slice(1)) {
-          case 'R0':
-          case 'R1':
-          case 'R2':
-          case 'R3':
-          case 'R4':
-          case 'R5':
-          case 'R6':
-          case 'R7':
-          case 'R8':
-          case 'R9':
-          case 'R10':
-          case 'R11':
-          case 'R12':
-          case 'R13':
-          case 'R14':
-          case 'R15':
-            // this can't be right, can it? Need to look up exact values, since then you would have SP being the same address as R0...
-            return `@${ instruction.slice(2) }`;
           case 'SP':
             return '@0';
           case  'LCL':
@@ -82,14 +76,164 @@ export class AssemblerService {
             return '@16384';
           case 'KBD':
             return '@24576';
-          default:
-            return this.parseVariable(instruction.slice(1));
         }
       }
+      return instruction;
     });
   }
 
-  private parseVariable(s: string) {
-    return undefined;
+  private parseVariables() {
+    this.variables = [];
+    this.instructions = this.instructions.map((instruction) => {
+      if (instruction.match(/[@][a-zA-Z]+/g)) {
+        const variable = this.variables.find(v => instruction.slice(1) === v.key);
+        if (variable) {
+          return `@${ variable.value }`;
+        }
+        this.variables.push({key: instruction.slice(1), value: this.variables.length + 16});
+        return `@${ this.variables[this.variables.length - 1].value }`;
+      }
+      return instruction;
+    });
+  }
+
+  private translate() {
+    this.machineCode = this.instructions.map(instruction => {
+      instruction.replace(' ', '');
+      if (instruction.startsWith('@')) {
+        return this.translateAInstruction(instruction.slice(1));
+      }
+      return this.translateCInstruction(instruction);
+    }).join('\n');
+  }
+
+  private translateAInstruction(instruction) {
+    if (!instruction.match(/\d+/gm)) {
+      return 'error - ' + instruction;
+    }
+    return (+instruction).toString(2).padStart(16, '0');
+  }
+
+  private translateCInstruction(instruction: string) {
+    let jump;
+    let comp;
+    let dest;
+    if (instruction.includes(';')) {
+      jump = instruction.slice(instruction.indexOf(';') + 1);
+      instruction = instruction.slice(0, instruction.indexOf(';'));
+    } else {
+      jump = '';
+    }
+    if (instruction.includes('=')) {
+      dest = instruction.slice(0, instruction.indexOf('='));
+      instruction = instruction.slice(instruction.indexOf('=') + 1);
+    } else {
+      dest = '';
+    }
+    comp = instruction;
+    return `111${ this.translateComp(comp) }${ this.translateDest(dest) }${ this.translateJump(jump) }`;
+  }
+
+  private translateComp(comp: any) {
+    switch (comp) {
+      case '0':
+        return '0101010';
+      case '1':
+        return '0111111';
+      case '-1':
+        return '0111010';
+      case 'D':
+        return '0001100';
+      case 'A':
+        return '0110000';
+      case 'M':
+        return '1110000';
+      case '!D':
+        return '0001101';
+      case '!A':
+        return '0110001';
+      case '!M':
+        return '1110001';
+      case '-D':
+        return '0001111';
+      case '-A':
+        return '0110011';
+      case '-M':
+        return '1110011';
+      case 'D+1':
+        return '0011111';
+      case 'A+1':
+        return '0110111';
+      case 'M+1':
+        return '1110111';
+      case 'D-1':
+        return '0001110';
+      case 'A-1':
+        return '0110010';
+      case 'M-1':
+        return '1110010';
+      case 'D+A':
+        return '0000010';
+      case 'D+M':
+        return '1000010';
+      case 'D-A':
+        return '0010011';
+      case 'D-M':
+        return '1010011';
+      case 'A-D':
+        return '0000111';
+      case 'M-D':
+        return '1000111';
+      case 'D&A':
+        return '0000000';
+      case 'D&M':
+        return '1000000';
+      case 'D|A':
+        return '0010101';
+      case 'D|M':
+        return '1010101';
+    }
+  }
+
+  private translateDest(dest: any) {
+    switch (dest) {
+      case '':
+        return '000';
+      case 'M':
+        return '001';
+      case 'D':
+        return '010';
+      case 'MD':
+        return '011';
+      case 'A':
+        return '100';
+      case 'AM':
+        return '101';
+      case 'AD':
+        return '110';
+      case 'AMD':
+        return '111';
+    }
+  }
+
+  private translateJump(jump: any) {
+    switch (jump) {
+      case '':
+        return '000';
+      case 'JGT':
+        return '001';
+      case 'JEQ':
+        return '010';
+      case 'JGE':
+        return '011';
+      case 'JLT':
+        return '100';
+      case 'JNE':
+        return '101';
+      case 'JLE':
+        return '110';
+      case 'JMP':
+        return '111';
+    }
   }
 }
